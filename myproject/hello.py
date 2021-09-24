f3ae291db020eaed3fc01cc86dd6e94acfe8a14c
TMY35 <crabtang@hotmail.com>
2021-09-24 09:22:27 -0400

    Add index page

---
+++ myproject/hello.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template
+from markupsafe import escape
+
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
+
+import os
+import sys
+
+name = 'Marvin'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
+
+app = Flask(__name__)
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
+
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
---
+++ myproject/run.sh
@@ -0,0 +1,5 @@
+source /home/marvin/myproject/venv/bin/activate
+export FLASK_ENV=development
+export FLASK_APP=hello
+flask run --host=0.0.0.0
+
